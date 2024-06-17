f43e4b509c66587c2034f06635e888b851f5ea82
Justin Garcia <justingarcia1901@gmail.com>
2024-06-17 17:20:41 -0400

    routes, controllers, moved problem/solution comments

--- index.js
+++ index.js
@@ -14,79 +14,13 @@ app.use(express.urlencoded({extended: false})); // Allow form data to add produc
 dbUser = process.env.DB_USER;
 dbPassword = process.env.DB_PASSWORD;
 
+// routes
+app.use('/api/products', productRoute);
 
 // settting up a get request
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
-
-// setting up a get request for the products
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json({ data: await Product.find() });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// setting up a get request for a single product
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        res.status(200).json({ data: product });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// setting up a post to create products
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(201).json({ data: product });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// setting up a put request to update a product
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        // if the product is not found, return a 404 error
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-        // return the updated product
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-//PROBLEM Cannot PUT /api/product/ | SOLUTION: Add a forward slash at the begining of the route
-
-// setting up a delete request to delete a product
-app.delete('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndDelete(id);
-
-        if (!product) {
-            return res.status(404).json({ message: 'Product not found' });
-        }
-        
-
-        res.status(200).json({ message: "Product deleted successfully"});
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
-//PROBLEM:"Cannot access 'product' before initialization" | SOLUTION: Define product (line 71) before the if statement
-
 
 
 // Connect to MongoDB
